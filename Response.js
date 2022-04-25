bfb24e78cb0d9161790c75e1a3c0b627a575fb44
NathansStuff <n.odonnell@hotmail.com>
2022-04-25 17:34:20 +1000

    connected to googleapi + dummy data

---
+++ Response.js
@@ -0,0 +1,208 @@
+export default {
+  kind: 'customsearch#search',
+  url: {
+    type: 'application/json',
+    template:
+      'https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json',
+  },
+  queries: {
+    request: [
+      {
+        title: 'Google Custom Search - testing',
+        totalResults: '5',
+        searchTerms: 'testing',
+        count: 5,
+        startIndex: 1,
+        inputEncoding: 'utf8',
+        outputEncoding: 'utf8',
+        safe: 'off',
+        cx: '43ba6fb1e515b2873',
+      },
+    ],
+  },
+  context: {
+    title: 'GoogleClone',
+  },
+  searchInformation: {
+    searchTime: 0.419687,
+    formattedSearchTime: '0.42',
+    totalResults: '5',
+    formattedTotalResults: '5',
+  },
+  items: [
+    {
+      kind: 'customsearch#result',
+      title: "How Google's Algorithm is Focused on Its Users - Google Search",
+      htmlTitle:
+        'How Google&#39;s Algorithm is Focused on Its Users - Google Search',
+      link: 'https://www.google.com/search/howsearchworks/mission/users/',
+      displayLink: 'www.google.com',
+      snippet:
+        "Learn how changes to Google's algorithm are tested with Google search ... Testing. Search has changed over the years to meet the evolving needs and ...",
+      htmlSnippet:
+        'Learn how changes to Google&#39;s algorithm are <b>tested</b> with Google search ... <b>Testing</b>. Search has changed over the years to meet the evolving needs and&nbsp;...',
+      cacheId: 'ObxN5sVIizUJ',
+      formattedUrl:
+        'https://www.google.com/search/howsearchworks/mission/users/',
+      htmlFormattedUrl:
+        'https://www.google.com/search/howsearchworks/mission/users/',
+      pagemap: {
+        metatags: [
+          {
+            referrer: 'no-referrer',
+            'twitter:title':
+              "How Google's Algorithm is Focused on Its Users - Google Search",
+            viewport: 'width=device-width, initial-scale=1.0',
+            'twitter:description':
+              "Learn how changes to Google's algorithm are tested with Google search experiments to ensure that we continue to provide the most useful results, and how ads help us keep Google Search free.",
+            'og:title':
+              "How Google's Algorithm is Focused on Its Users - Google Search",
+            'og:description':
+              "Learn how changes to Google's algorithm are tested with Google search experiments to ensure that we continue to provide the most useful results, and how ads help us keep Google Search free.",
+          },
+        ],
+      },
+    },
+    {
+      kind: 'customsearch#result',
+      title: 'Rigorous Testing – How Google Search Works',
+      htmlTitle: 'Rigorous <b>Testing</b> – How Google Search Works',
+      link: 'https://www.google.com/search/howsearchworks/how-search-works/rigorous-testing/',
+      displayLink: 'www.google.com',
+      snippet:
+        'Improving Search with rigorous testing. Our goal is always to provide you with the most useful and relevant information. Any changes we make to Search are ...',
+      htmlSnippet:
+        'Improving Search with rigorous <b>testing</b>. Our goal is always to provide you with the most useful and relevant information. Any changes we make to Search are&nbsp;...',
+      cacheId: '4VYSKZ0ZDVQJ',
+      formattedUrl:
+        'https://www.google.com/search/howsearchworks/how.../rigorous-testing/',
+      htmlFormattedUrl:
+        'https://www.google.com/search/howsearchworks/how.../rigorous-<b>testing</b>/',
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGS3cygt5kGAiZY-7hO8fSAHJHy5cC3mlOzc5Dbg3h6qghrry_RtnEAEo',
+            width: '310',
+            height: '163',
+          },
+        ],
+        metatags: [
+          {
+            'og:image':
+              'https://lh3.googleusercontent.com/dnpoiu9aopdadx61b_R8SlKyCHQIBNSTlYKOqUdZ1J8RPl3dGu1i57tmb7KzVEgpjdD_wc-CFirNtxU3wDiS9b1ZNAYxqq4tyojt5X4Q',
+            'og:type': 'website',
+            'twitter:card': 'summary',
+            'twitter:title': 'Rigorous Testing – How Google Search Works',
+            'og:site_name': 'Google Search - Discover How Google Search Works',
+            'og:title': 'Rigorous Testing – How Google Search Works',
+            'og:description':
+              "Our goal is always to provide you with the most useful and relevant information. Any changes we make to Search are always to improve the usefulness of results you see. That's why we never accept payment from anyone to be included in Search results.",
+            'og:image:secure_url':
+              'https://lh3.googleusercontent.com/dnpoiu9aopdadx61b_R8SlKyCHQIBNSTlYKOqUdZ1J8RPl3dGu1i57tmb7KzVEgpjdD_wc-CFirNtxU3wDiS9b1ZNAYxqq4tyojt5X4Q',
+            'twitter:image':
+              'https://lh3.googleusercontent.com/dnpoiu9aopdadx61b_R8SlKyCHQIBNSTlYKOqUdZ1J8RPl3dGu1i57tmb7KzVEgpjdD_wc-CFirNtxU3wDiS9b1ZNAYxqq4tyojt5X4Q',
+            referrer: 'no-referrer',
+            viewport: 'width=device-width, initial-scale=1.0',
+            'twitter:description':
+              "Our goal is always to provide you with the most useful and relevant information. Any changes we make to Search are always to improve the usefulness of results you see. That's why we never accept payment from anyone to be included in Search results.",
+            'og:locale': 'en',
+            'og:url':
+              'https://www.google.com/search/howsearchworks/how-search-works/rigorous-testing/',
+          },
+        ],
+        cse_image: [
+          {
+            src: 'https://lh3.googleusercontent.com/dnpoiu9aopdadx61b_R8SlKyCHQIBNSTlYKOqUdZ1J8RPl3dGu1i57tmb7KzVEgpjdD_wc-CFirNtxU3wDiS9b1ZNAYxqq4tyojt5X4Q',
+          },
+        ],
+      },
+    },
+    {
+      kind: 'customsearch#result',
+      title: 'testing tool in Google Search Console',
+      htmlTitle: '<b>testing</b> tool in Google Search Console',
+      link: 'https://www.google.com/webmasters/tools/robots-testing-tool',
+      displayLink: 'www.google.com',
+      snippet:
+        'Use Search Console to monitor Google Search results data for your properties.',
+      htmlSnippet:
+        'Use Search Console to monitor Google Search results data for your properties.',
+      formattedUrl:
+        'https://www.google.com/webmasters/tools/robots-testing-tool',
+      htmlFormattedUrl:
+        'https://www.google.com/webmasters/tools/robots-<b>testing</b>-tool',
+      pagemap: {
+        metatags: [
+          {
+            viewport: 'width=device-width, initial-scale=1',
+            'format-detection': 'telephone=no',
+          },
+        ],
+      },
+    },
+    {
+      kind: 'customsearch#result',
+      title: 'Google',
+      htmlTitle: 'Google',
+      link: 'https://www.google.com/',
+      displayLink: 'www.google.com',
+      snippet:
+        "Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking ...",
+      htmlSnippet:
+        'Search the world&#39;s information, including webpages, images, videos and more. Google has many special features to help you find exactly what you&#39;re looking&nbsp;...',
+      cacheId: 'y14FcUQOGl4J',
+      formattedUrl: 'https://www.google.com/',
+      htmlFormattedUrl: 'https://www.google.com/',
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT3X9ZpkcNrr2kAcGZuXPZTNiK1oeTXhRMCZ1UGxNLf-an4du0eriX01mc',
+            width: '320',
+            height: '112',
+          },
+        ],
+        metatags: [
+          {
+            referrer: 'origin',
+            viewport: 'width=device-width,minimum-scale=1.0',
+            google: 'notranslate',
+            'format-detection': 'telephone=no',
+          },
+        ],
+        cse_image: [
+          {
+            src: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png',
+          },
+        ],
+      },
+    },
+    {
+      kind: 'customsearch#result',
+      title: 'Android Beta Program',
+      htmlTitle: 'Android Beta Program',
+      link: 'https://www.google.com/android/beta',
+      displayLink: 'www.google.com',
+      snippet:
+        'Android Beta for Pixel offers you a simple way to try pre-release versions of Android, and test drive our new features. The feedback you provide will help ...',
+      htmlSnippet:
+        'Android Beta for Pixel offers you a simple way to try pre-release versions of Android, and <b>test</b> drive our new features. The feedback you provide will help&nbsp;...',
+      cacheId: 'AUFDLyB7DUsJ',
+      formattedUrl: 'https://www.google.com/android/beta',
+      htmlFormattedUrl: 'https://www.google.com/android/beta',
+      pagemap: {
+        metatags: [
+          {
+            'application-name': 'Android Beta Program',
+            referrer: 'origin',
+            'apple-mobile-web-app-status-bar-style': 'black',
+            'msapplication-tap-highlight': 'no',
+            viewport: 'width=device-width, initial-scale=1.0',
+            'apple-mobile-web-app-capable': 'yes',
+            'apple-mobile-web-app-title': 'Android Beta Program',
+            'mobile-web-app-capable': 'yes',
+          },
+        ],
+      },
+    },
+  ],
+}
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,15 @@
 import Head from 'next/head'
-import Header from '../components/Header'
+import { GetServerSideProps } from 'next'
 
-export default function Search() {
+import Header from '../components/Header'
+import Response from '../Response'
+
+interface Props {
+  results: {}
+}
+
+export default function Search({ results }: Props) {
+  console.log( results )
   return (
     <div>
       <Head>
@@ -11,3 +19,20 @@ export default function Search() {
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const useDummyData = true
+
+  const data = useDummyData ? Response : await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${context.query.term}`
+  ).then((response) => response.json())
+
+  // After SSR, pass props to client
+  return {
+    props: {
+      results: data,
+    },
+  }
+}
+
+// https://www.googleapis.com/customsearch/v1?key=AIzaSyCwEYovkISzQ2iyOB9SILK6qm1WDwnpum8&cx=43ba6fb1e515b2873&q=testing
